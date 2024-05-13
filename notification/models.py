bf74e862b525b7bafc48d4feeae849f9650dccdf
yusof_antar <youssof.antar@gmail.com>
2024-05-13 20:25:36 +0300

    Notification Fixes

--- notification/models.py
+++ notification/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from users.models import CustomUser
+from farm.models import Farm
 
 
 class Notification(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=10000)
     user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    farm_id = models.ForeignKey(Farm, on_delete=models.CASCADE)
     created_at = models.DateField(auto_now=True)
 
     class Meta:
@@ -18,6 +20,7 @@ class Notification(models.Model):
         return {
             'title': self.title,
             'description': self.description,
-            'user_id': self.farm_id,
+            'user_id': self.user_id_id,
+            'farm': self.farm_id.title,
             'created_at': self.created_at,
         }
--- notification/serializer.py
+++ notification/serializer.py
@@ -1,19 +1,25 @@
 from rest_framework import serializers
-from .models import Notification, CustomUser
+from .models import Notification, CustomUser, Farm
 
 
 class NotificationSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     description = serializers.CharField(max_length=10000)
     user_id = serializers.CharField(max_length=255)
+    farm_id = serializers.CharField(max_length=255)
 
     def validate(self, attrs):
         user = CustomUser.objects.filter(id=attrs['user_id'])
+        farm = Farm.objects.filter(id=attrs['farm_id'])
         print(user)
         if not user:
             raise serializers.ValidationError(
                 "User Not Found, Please Login")
+        if not farm:
+            raise serializers.ValidationError(
+                'Farm Not Found, Please Check again Later or With Support')
         attrs['user_id'] = user[0]
+        attrs['farm_id'] = farm[0]
         return attrs
 
     def create(self, validated_data):
@@ -22,7 +28,8 @@ class NotificationSerializer(serializers.Serializer):
             notification = Notification.objects.create(
                 title=validated_data['title'],
                 description=validated_data['description'],
-                user_id=validated_data['user_id']
+                user_id=validated_data['user_id'],
+                farm_id=validated_data['farm_id'],
             )
             print("damn")
         except:
